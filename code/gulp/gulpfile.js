694869bf5df6dafb6a42e0b78a5b8fd436347bbb
keke.yu <keke.yu@17zuoye.com>
2020-06-09 19:18:23 +0800

    gulp

--- code/gulp/gulpfile.js
+++ code/gulp/gulpfile.js
@@ -1,5 +1,8 @@
 const fs = require("fs")
-
+const { Transform } = require('stream')
+const cleanCss = require("gulp-clean-css")
+const rename = require("gulp-rename")
+const { src, dest } = require('gulp')
 //gulp的基本使用
 // exports.foo = done => {
 //   console.log('foo')
@@ -44,25 +47,25 @@ const fs = require("fs")
 //   console.log('callback~~')
 //   done(new Error('task failed!'))
 // }
-exports.promise = done =>{
-  console.log('promise')
-  return Promise.resolve()
-}
-exports.promise_error = done =>{
-  console.log('promise')
-  return Promise.reject(new Error('task failed!'))
-}
+// exports.promise = done =>{
+//   console.log('promise')
+//   return Promise.resolve()
+// }
+// exports.promise_error = done =>{
+//   console.log('promise')
+//   return Promise.reject(new Error('task failed!'))
+// }
 
-const timeout = time =>{
-  return new Promise(resolve=>{
-    setTimeout(resolve,time)
-  })
-}
+// const timeout = time =>{
+//   return new Promise(resolve=>{
+//     setTimeout(resolve,time)
+//   })
+// }
 
-exports.async = async()=>{
-  await timeout(3000)
-  console.log("async~~~")
-}
+// exports.async = async()=>{
+//   await timeout(3000)
+//   console.log("async~~~")
+// }
 
 // exports.stream = ()=>{
 //   const readStream = fs.createReadStream("package.json")
@@ -70,12 +73,42 @@ exports.async = async()=>{
 //   readStream.pipe(writeStream)
 //   return readStream
 // }
-exports.stream = done=>{
-  const readStream = fs.createReadStream("package.json")
-  const writeStream = fs.createWriteStream("temp2.txt")
-  readStream.pipe(writeStream)
-  readStream.on('end',()=>{
-    done()
-  })
+// exports.stream = done=>{
+//   //文件读取流
+//   const readStream = fs.createReadStream("package.json")
+//    //文件写入流
+//   const writeStream = fs.createWriteStream("temp2.txt")
+//   //把读取出来的文件流导入写入文件流
+//   readStream.pipe(writeStream)
+//   readStream.on('end',()=>{
+//     done()
+//   })
+// }
+
+// exports.default = () =>{
+//   const read = fs.createReadStream('style.css')
+//   const write = fs.createWriteStream('style.min.css')
+//   //文件转换流
+//  const transform = new Transform({
+//     transform:(chunk,encoding,callback) =>{
+//       const input = chunk.toString()
+//       const output = input.replace(/\s+/g,'').replace(/\/\*.+?\*\//g,'')
+//       callback(null,output)
+//     }
+
+//   })
+//   read
+//   .pipe(transform)
+//   .pipe(write)
+//   return read
+// }
+
+exports.default = () =>{
+  return src('*.css')
+         .pipe(cleanCss())
+         .pipe(rename({extname:'.main.css'}))
+         .pipe(dest("dist"))
 }
+
+
 
